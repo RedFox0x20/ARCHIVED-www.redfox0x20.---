cd0d8405b31bd52bbcab3d6cdeccbbf3d9223f8e
Red (RedFox0x20) <redfox0x20@gmail.com>
2021-09-01 12:05:58 +0100

    fix

--- components/header.js
+++ components/header.js
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 export default function Header() {
     return (
         <div className="navbar mb-2 shadow-lg bg-neutral text-neutral-content rounded-box">
@@ -6,16 +8,13 @@ export default function Header() {
             </div>
             <div className="flex-1 px-2 mx-2">
                 <div className="items-stretch hidden md:flex">
-                    <a
-                        className="btn btn-ghost btn-sm rounded-btn"
-                        href="/"
-                        target="_blank"
-                    >
+                    <Link className="btn btn-ghost btn-sm rounded-btn" href="/">
                         {'Home'}
-                    </a>
+                    </Link>
                     <a
                         className="btn btn-ghost btn-sm rounded-btn"
                         href="https://m7tws.redfox0x20.xyz/"
+                        rel="noreferrer"
                         target="_blank"
                     >
                         {'Amateur Radio'}
